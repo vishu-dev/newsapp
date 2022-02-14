d5aea0f5570514d1e86e7622ccc9b0b3abe576d7
vishal <vj00273@gmail.com>
2022-02-14 15:17:16 +0530

    newsapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
-export default App;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+export default class App extends Component {
+  pageSize = 10;
+
+  state = {
+    progress:0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({progress: progress})
+  }
+
+  render() {
+    return <div>
+      <Router>
+      <Navbar/>
+      <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+      />
+      <Switch>
+          <Route exact path="/"><News setProgrees={this.setProgrees}  key="general" pageSize={this.pageSize} country="in" category="general"/></Route>
+          <Route exact path="/business"><News setProgrees={this.setProgrees}  key="business" pageSize={this.pageSize} country="in" category="business"/></Route>
+          <Route exact path="/entertainment"><News setProgrees={this.setProgrees}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route>
+          <Route exact path="/general"><News setProgrees={this.setProgrees}  key="gerneral" pageSize={this.pageSize} country="in" category="general"/></Route>
+          <Route exact path="/health"><News setProgrees={this.setProgrees}  key="health" pageSize={this.pageSize} country="in" category="health"/></Route>
+          <Route exact path="/science"><News setProgrees={this.setProgrees}  key="science" pageSize={this.pageSize} country="in" category="science"/></Route>
+          <Route exact path="/sports"><News setProgrees={this.setProgrees}  key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route>
+          <Route exact path="/technology"><News setProgrees={this.setProgrees}  key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route>
+        </Switch> 
+      </Router>
+    </div>
+  }
+}
